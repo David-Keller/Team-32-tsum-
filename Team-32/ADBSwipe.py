1acd05eeb38ce9d17a4ef1d1c85f41527ae8e2c2
Nret <shivers9000@gmail.com>
2016-10-14 01:28:40 -0700

    works for big commands too
    example 'unlock device code'

--- Team-32/ADBSwipe.py
+++ Team-32/ADBSwipe.py
@@ -1,19 +1,14 @@
 #! python3
 
-"""
-Plan of Attack:
-construct the string to send over adb
-send the string over adb
-
-
-"""
-
 import os
 from numpy import linspace
 
-command = ""
+locationToPushTo = "/data/local/tmp/"
 eventDevice = "/dev/input/event0"
-adb = "adb shell "
+
+adb = "adb "
+shell = "shell "
+adbShell = adb + shell
 
 def startTouch():
     return "sendevent {device} 3 57 1337;sendevent {device} 1 330 1;sendevent {device} 1 325 1;".format(device = eventDevice)
@@ -28,36 +23,86 @@ def swipe(coords):
     return commands
 
 def run(command):
-    print("running {}{}".format(adb, command))
-    return os.popen(adb + command).read()
+    """ adb has a hard limit of 4096 bytes that can be sent through, write to file and push file to get around that """
+    if type(command) is Command:
+        command = command.str()
+    print("running {}".format(command))
+    with open("command", "w") as o:
+        o.write(command)
+    os.popen(adb + "push command " + locationToPushTo)
+    return os.popen(adbShell + "sh " + locationToPushTo + "command").read()
 
-def line(*coords, num=10):
+def runMultiple(command):
+    if type(command) is Command:
+        command = command.str()
+    # split command into just under max length and send those
+
+def runSimple(command):
+    """ Used for running single (or short) commands over adb """
+    if type(command) is Command:
+        command = command.str()
+    print("running {}{}".format(adbShell, command))
+    return os.popen(adbShell + command).read()
+
+def line(*coords, num=2):
     """ Given 2 (x, y) coords, returns a list of coords that draws a line between the two including the ends """
     xs = linspace(coords[0][0], coords[1][0], num=num, dtype=int)
     ys = linspace(coords[0][1], coords[1][1], num=num, dtype=int)
     return list(zip(xs, ys))
 
+def getScreenSize():
+    """ Note this requires the android device to have wm """
+    output = runSimple("wm size")
+    output = output[:-1] # Remove the trailing newline
+    output = output.split(": ")[1] # extract just the WidthxHeight
+    output = output.split("x") # extract the width and height into a list
+    return output
+
+def sleep(time=.5):
+    return "sleep {};".format(time)
+
+def multiSwipe(listOfSwipes):
+
+    return
+
+class Command:
+    commandString = ""
+
+    def add(self, command):
+        self.commandString += str(command)
+    def str(self):
+        return self.commandString
+    def reset(self):
+        self.commandString = ""
+
+"""
+[(x1,y1), (x2, y2), (x3, y3)]
+
+"""
+
+cmd = Command()
+
 # wake device
-run("input keyevent 26")
+runSimple("input keyevent 26")
 
-commands = startTouch()
+# swipe up
+cmd.add(startTouch())
+cmd.add(swipe(line((300, 2000), (900, 500), num=10)))
+cmd.add(endTouch())
 
-coords = line((600, 2000), (600, 500))
-commands += swipe(coords)
+cmd.add(sleep())
 
-commands += endTouch()
+# lock code
+cmd.add(startTouch())
+# bottom row
+cmd.add(swipe(line((340, 2100), (1100, 2000))))
+# up one
+cmd.add(swipe(line((1100, 2000), (1120, 1700))))
+# up left
+cmd.add(swipe(line((1120, 1700), (700, 1300))))
+cmd.add(endTouch())
 
-commands += "sleep 1;"
-
-commands += startTouch()
-
-coords = line((600, 500), (600, 1000))
-commands += swipe(coords)
-
-commands += endTouch()
-
-
-run(commands)
+run(cmd)
 
 
 
@@ -81,8 +126,8 @@ ABS_MT_TOUCH_MINOR (49) - minor axis of the width of the finger?
 ABS_MT_PRESSURE (58) - pressure of the touch
 SYN_MT_REPORT (2) - end of separate touch data
 SYN_REPORT (0) - end of report
-BTN_TOUCH (330) - use value of 1
-BTN_TOOL_FINGER (325) - use value of 1
+BTN_TOUCH (330) - touch down/up
+BTN_TOOL_FINGER (325) - finger down/up
 
 Let's say we want to emulate a touch down event at the point with coordinates x=300, y=400. We will need to execute the following sendevent commands:
 
